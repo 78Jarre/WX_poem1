2e5f6ec12bc609647cb777a2a94fd274d2b7282e
Chenyq <2118424781@qq.com>
2023-11-01 20:49:11 +0800

    第一次提交

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import json
+
+from collections import OrderedDict
 
 from flask import Flask
 from flask import request
-from flask.views import MethodView
+from flask import jsonify
+from flask import Response
+
 from flask_cors import CORS
 
 from extension import db
-from models import Poem
+from models import Poem, Poetry
 
 app = Flask(__name__)
 #  跨域
@@ -20,14 +25,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 拓展插件对象绑定到程序实例
 db.init_app(app)
 
-
-# @app.cli.commands()
-# def create():
-#     db.drop_all()
-#     db.create_all()
-#     Poem.init_db()
-# with app.app_context():
-#     db.create_all()
+# 建立表格
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -38,39 +38,42 @@ def hello_world():  # put application's code here
 def poem_list():
     q_list = db.select(Poem).order_by(Poem.poem_id)
     poems = db.session.execute(q_list).scalars()
-    return {"message":"ok", "data":[poem.json() for poem in poems]}
+    return {"message": "ok", "data": [poem.json() for poem in poems]}
 
-@app.get('/poem/<int:poem_id>')
-def poem_detail(poem_id):
-    poem = db.get_or_404(Poem, poem_id)
 
-@app.get('/poem/<int:id>')
-def poem_detail(id):
-    poem = db.select(Poem).where(Poem.poem_id == id).first()
-    return {"message":"ok", "data":poem.json()}
+@app.get('/poem/<int:pid>')
+def poem_detail(pid):
+    poem = db.get_or_404(Poem, pid)
+    return Response(json.dumps(poem),mimetype='application/json')
+
+
+# @app.get('/poem/<int:pid>')
+# def poem_detail(pid):
+#     poem = db.select(Poem).where(Poem.poem_id == pid).first()
+#     return {"message":"ok", "data":poem.json()}
 
 @app.post('/poem')
 def poem_create():
     data = request.get_json()
-    poem = Poem(poem_name = data.get("poem_name"),poem_author=data.get("poem_author"),poem_content=data.get("poem_content"))
+    poem = Poem(poem_name=data.get("poem_name"), poem_author=data.get("poem_author"),
+                poem_dynasty=data.get("poem_dynasty"), poem_type=data.get("poem_type"),
+                poem_content=data.get("poem_content"))
     db.session.add(poem)
     db.session.commit()
-    return {"message":"ok", "data":poem.json()}
+    return {"message": "ok", "data": poem.json()}
 
-@app.delete('/poem/<int:id>')
-def poem_delete(id):
-    poem = db.get_or_404(Poem, id)
+
+@app.delete('/poem/<int:pid>')
+def poem_delete(pid):
+    poem = db.get_or_404(Poem, pid)
     db.session.delete(poem)
     db.session.commit()
-    return {"message":"ok"}
+    return {"message": "ok"}
 
-
-
-
-
+@app.get('/poetry/<string:PoetryName>')
+def Poetry_biology(PoetryName):
+    poetry_content = db.get_or_404(Poetry, PoetryName)
+    return Response(json.dumps(poetry_content),mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import requests
 
+
+poem1 = {
+    'poem_name':'静夜思',
+    'poem_author':'李白',
+    'poem_dynasty':'唐',
+    'poem_type':'五言绝句',
+    'poem_content':'床前明月光，疑是地上霜。举头望明月，低头思故乡。'
+}
+
+# with requests.post('http://127.0.0.1:5000/poem',json=poem1) as response:
+#     print(response.json())
+
+
 with requests.get('http://127.0.0.1:5000/poem') as response:
     print(response.json())
+#
+#
+# with requests.delete('http://127.0.0.1:5000/poem/1') as response:
+#     print(response.text)
